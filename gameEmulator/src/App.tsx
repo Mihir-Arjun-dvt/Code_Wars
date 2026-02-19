193842b31e20b226cefdba42ab09ffedd88a2d7e
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-19 13:56:31 +0200

    tailwind is now set up

--- gameEmulator/src/App.tsx
+++ gameEmulator/src/App.tsx
@@ -1,8 +1,12 @@
-
+import './index.css'
 function App() {
 
   return (
-    
+    <>
+    <div className="bg-red-500 text-red-900">
+          Hello
+    </div>
+    </> 
   )
 }
 
---
+++ gameEmulator/src/components/grid.tsx
@@ -0,0 +1,10 @@
+export default function Grid(){
+    return(
+        <>
+            <div>
+                
+            </div>
+
+        </>
+    )
+}
